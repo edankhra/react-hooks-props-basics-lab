09b5e1ffc32789239254c3a1c02de6aebd7df729
Ekta <edankhra@gmail.com>
2023-08-01 07:47:35 -0400

    completed

--- src/components/Links.js
+++ src/components/Links.js
@@ -15,8 +15,8 @@ const Links = ({ github, linkedin }) => {
     return (
       <div>
         <h3>Links</h3>
-        <a href={"https://github.com/liza"} target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href={"https://www.linkedin.com/in/liza/"} target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href={"https://github.com/liza"} target="_blank" rel="noopener noreferrer">{github}</a>
+        <a href={"https://www.linkedin.com/in/liza/"} target="_blank" rel="noopener noreferrer">{linkedin}</a>
       </div>
     );
   };
